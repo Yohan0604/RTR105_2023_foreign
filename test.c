6f949151f8f88478f4d8b8673d27cb6def0823c1
Yohan0604 <Yohan.Rajapaksha@edu.rtu.lv>
2023-11-01 19:29:32 +0200

    test1

---
+++ test.c
@@ -0,0 +1,16 @@
+
+
+#include<stdio.h>
+int main()
+{
+long long int num1,num2;
+printf("Enter the first number: ");
+scanf("%lld",&num1);
+
+printf("Enter the second number: ");
+scanf("%lld",&num2);
+
+long long int result=num1*num2;
+printf("The multiplication result is :%lld\n",result);
+return 0;
+}
