e8ccc5e87b8fd6b7d21f90050dc654847be7b089
Yohan0604 <144117817+Yohan0604@users.noreply.github.com>
2023-11-16 19:36:46 +0200

    Update test2.c

--- class_13/test2.c
+++ class_13/test2.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
+
 int main()
 {
 unsigned char num;
 printf("Please enter a natural number (0-255):");
 
 scanf("%hhu",&num);
-printf("Binary representation:");
-for (int i =7; i>=0; i--){
-int bit=(num>>i)& 1;
-printf("%d",bit);
-}
+printf("Binary representation:%hhu",((num&0x80)>>7));
+printf("%hhu",((num&0x40)>>6));
+printf("%hhu",((num&0x20)>>5));
+printf("%hhu",((num&0x10)>>4));
+printf("%hhu",((num&8)>>3));
+printf("%hhu",((num&4)>>2));
+printf("%hhu",((num&2)>>1));
+printf("%hhu",((num&1)>>0));
+
 printf("\n");
-return 0;
-}
+
