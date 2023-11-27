bb6ebaa15d6a2ed252f309b3aee10dfd21ef1612
Yohan0604 <Yohan.Rajapaksha@edu.rtu.lv>
2023-11-27 20:56:08 +0200

    test3

---
+++ if.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{ char a,b,c;
+char order;
+printf("Enter three letters :");
+scanf("%c,%c,%c ",&a,&b,&c);
+printf("Enter A for alpahabet order or Enter R  for opposite order: ");
+scanf("%c",&order);
+
+
+
+if (order=='A' || order=='a')
+{
+if(a>b)
+{char swap =a;
+a=b;
+b=swap;
+}
+if(b>c)
+{char swap =b;
+b=c;
+c=swap;
+}
+printf("The sorted sequence is: %c %c %c\n",a,b,c);
+}
+else if (order =='R' || order =='r')
+{
+if(a<b)
+{char swap =a;
+a=b;
+b=swap;
+}
+if (b<c)
+{char swap=b;
+b=c;
+c=swap;
+}
+printf("The sorted sequence is: %c %c %c\n",a,b,c);
+}
+
+return 0;
+}
---
+++ if1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+    char a, b, c; 
+    char order; 
+    printf("Enter three letters: "); 
+    scanf("%c %c %c", &a, &b, &c);
+    printf("Enter A for alphabet order or R for reverse order:");
+    scanf(" %c", &order); 
+    if (order == 'A' || order == 'a') 
+    {
+        
+        if (a > b) 
+        {
+            char swap = a; 
+            a = b;
+            b = swap;
+        }
+        if (b > c)
+        {
+            char swap = b; 
+            b = c;
+            c = swap;
+        }
+        if (a > b) 
+        {
+            char swap = a;
+            a = b;
+            b = swap;
+        }
+        printf("The sorted sequence is: %c %c %c\n", a, b, c); 
+    }
+    else if (order == 'R' || order == 'r') 
+    {
+       
+        if (a < b) 
+        {
+            char swap = a;
+            a = b;
+            b = swap;
+        }
+        if (b < c) 
+        {
+            char swap = b;
+            b = c;
+            c = swap;
+        }
+        if (a < b) 
+        {
+            char swap = a; 
+            a = b;
+            b = swap;
+        }
+        printf("The sorted sequence is: %c %c %c\n", a, b, c); 
+    }
+    
+ 
+    return 0;
+}
